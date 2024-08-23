461e5b864010d5599e0996e8c400529afe250bf7
riikka <riikaka@metropolia.fi>
2024-08-23 17:15:38 +0300

    Moduuli2

---
+++ Tehtava2.6.py
@@ -0,0 +1,7 @@
+import random
+
+print("Arvotaan kolminumeroinen koodi: ")
+print(f"{random.randint(0,9)} {random.randint(0,9)} {random.randint(0,9)}")
+
+print("Arvotaan nelinumeroinen koodi: ")
+print(f"{random.randint(1,6)} {random.randint(1,6)} {random.randint(1,6)} {random.randint(1,6)}")
